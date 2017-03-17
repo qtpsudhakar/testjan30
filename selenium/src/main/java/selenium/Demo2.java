65c39114891b6e65717748bee359dd29b7c02dc1
sudhakar <me@qtpsudhakar.com>
2017-03-17 08:39:13 +0530

    demo2 hello

--- selenium/src/main/java/selenium/Demo2.java
+++ selenium/src/main/java/selenium/Demo2.java
@@ -5,6 +5,10 @@ public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("this is demo2");
 		System.out.println("123");
+		System.out.println("hello");
+		
+		
+		
 	}
 
 }
