31fc41603e5f9b087eaa1d792ae0f7ebe17b4be5
sudhakar <me@qtpsudhakar.com>
2017-03-17 08:28:35 +0530

    second

--- selenium/src/main/java/selenium/Demo1.java
+++ selenium/src/main/java/selenium/Demo1.java
@@ -5,6 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("this is maven project");
 		System.out.println("first change");
+		System.out.println("second");
 	}
 
 }
